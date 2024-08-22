e0d7d9647c41921c359e568f56f99adf25d1a303
HollowHolderOfHeaven <zachary.fraser7@education.nsw.gov.au>
2024-08-22 12:57:02 +1000

    Tidied up code and fixxed bugs and added more

--- Car.py
+++ Car.py
@@ -3,42 +3,58 @@ from machine import Pin, PWM, time_pulse_us
 from time import sleep
 
 class Car:
-    #Code for the 1st motor pins
-    motor1Enable = PWM(Pin(6, Pin.OUT))
-    motor1PositivePin = Pin(5, Pin.OUT)
-    motor1Negativepin = Pin(4, Pin.OUT)
+    
+    def __init__(self) -> None:
+        self.LED1 = Pin()
+        self.LED2 = Pin()
+        self.LED3 = Pin()
+        self.LED4 = Pin()
+    
+        self.sleep = Pin()
+        
+        
+    #Motors code
+    def Motors():
+        #Code for the 1st motor pins
+        motor1Enable = PWM(Pin(6, Pin.OUT))
+        motor1PositivePin = Pin(5, Pin.OUT)
+        motor1Negativepin = Pin(4, Pin.OUT)
 
-    #Code for the 2nd motor pins
-    motor2Enable = PWM(Pin(8, Pin.OUT))
-    motor2PositivePin = Pin(7, Pin.OUT)
-    motor2Negativepin = Pin(4, Pin.OUT)
+        #Code for the 2nd motor pins
+        motor2Enable = PWM(Pin(8, Pin.OUT))
+        motor2PositivePin = Pin(7, Pin.OUT)
+        motor2Negativepin = Pin(4, Pin.OUT)
 
-    #Setting speed of the 1st motor
-    motor1Enable.freq(1000)
-
-
-    motor1Enable.duty_u16(65535)
-    motor1PositivePin.off()
-    motor1Negativepin.on()
-
-    #Setting speed of the 2nd motor
-    motor2Enable.freq(1000)
+        #Setting speed of the 1st motor
+        motor1Enable.freq(1000)
 
 
-    motor2Enable.duty_u16(65535)
-    motor2PositivePin.off()
-    motor2Negativepin.on()
+        motor1Enable.duty_u16(65535)
+        motor1PositivePin.off()
+        motor1Negativepin.on()
 
-    #temporary code to turn off motors after 5 seconds for testing
-    sleep(5)
+        #Setting speed of the 2nd motor
+        motor2Enable.freq(1000)
 
-    motor1Enable.duty_u16(0)
+
+        motor2Enable.duty_u16(65535)
+        motor2PositivePin.off()
+        motor2Negativepin.on()
+
+        #temporary code to turn off motors after 5 seconds for testing
+        sleep(5)
+
+        motor1Enable.duty_u16(0)
     
     #LED Pins
-    LED1 = Pin(9, Pin.OUT)
-    LED2 = Pin(10, Pin.OUT)
-    LED3 = Pin(11, Pin.OUT)
-    LED4 = Pin(12, Pin.OUT)
+    def LEDs(self):
+        
+        sleep(0.000001)
+        self.LED1 = Pin(9, Pin.OUT)
+        self.LED2 = Pin(10, Pin.OUT)
+        self.LED3 = Pin(11, Pin.OUT)
+        self.LED4 = Pin(12, Pin.OUT)
+
 
     #Below is the code for the ultrasonic senor
 
@@ -49,17 +65,34 @@ class Car:
 
     #Ultrasonic sensor activation
     while True:
-    
-    triggerPin.low()
-    sleep(0.05)
+            
+        triggerPin.low()
+        sleep(0.05)
 
-    triggerPin.high()
-    sleep(0.01)
-    triggerPin.low()
-    
-    duration = time_pulse_us(echoPin, 1)
-    distance_cm = 340 * duration / 20000
+        triggerPin.high()
+        sleep(0.01)
+        triggerPin.low()
+            
+        duration = time_pulse_us(echoPin, 1)
+        distance_cm = 340 * duration / 20000
+        if distance_cm < 20:
+            LED1.on()
+        else:
+            LED1.off()
+        if distance_cm < 15:
+            LED2.on()
+        else:
+            LED2.off()
+        if distance_cm < 10:
+            LED3.on()
+        else:
+            LED3.off()
+        if distance_cm < 5:
+            LED4.on()
+        else:
+            LED4.off()
+            
+            
+        
     
     #Print to test if Ultrasonic sensor is working (temporary)
-    print (distance_cm)
-    sleep(0.5)
